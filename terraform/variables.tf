d91af34471dd89f413ab7e004b5964ddeb2f281f
ww24 <ww24@users.noreply.github.com>
2021-05-09 23:18:20 +0900

    ci: add CI/CD workflows

---
+++ terraform/variables.tf
@@ -0,0 +1,46 @@
+variable "location" {
+  type    = string
+  default = "asia-northeast1"
+}
+
+variable "project" {
+  type = string
+}
+
+// credentials json value
+variable "google_credentials" {
+  type = string
+}
+
+variable "name" {
+  type    = string
+  default = "linebot"
+}
+
+variable "gar_repository" {
+  type    = string
+  default = "ww24"
+}
+
+variable "image_name" {
+  type    = string
+  default = "linebot"
+}
+
+variable "image_tag" {
+  type    = string
+  default = "latest"
+}
+
+// application environments
+variable "line_channel_secret" {
+  type = string
+}
+
+variable "line_channel_access_token" {
+  type = string
+}
+
+variable "allow_conv_ids" {
+  type = string
+}
