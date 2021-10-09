3d056fde7387e1bde1b11ebfc109b4ed8cd2b1c5
ww24 <ww24@users.noreply.github.com>
2021-10-10 02:21:11 +0900

    refactor(presentation/http): add http handler

---
+++ presentation/http/handler.go
@@ -0,0 +1,81 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/opentelemetry-operations-go/propagator"
+	"github.com/google/wire"
+	"go.opentelemetry.io/otel/propagation"
+	"go.uber.org/zap"
+
+	"github.com/ww24/linebot/domain/service"
+	"github.com/ww24/linebot/logger"
+	"github.com/ww24/linebot/usecase"
+)
+
+// Set provides a wire set.
+var Set = wire.NewSet(
+	NewHandler,
+	wire.Bind(new(http.Handler), new(*Handler)),
+)
+
+var prop = propagator.New()
+
+type Handler struct {
+	log          *logger.Logger
+	bot          service.Bot
+	eventHandler usecase.EventHandler
+	middlewares  []func(http.Handler) http.Handler
+}
+
+func NewHandler(
+	log *logger.Logger,
+	bot service.Bot,
+	eventHandler usecase.EventHandler,
+) *Handler {
+	return &Handler{
+		log:          log,
+		bot:          bot,
+		eventHandler: eventHandler,
+		middlewares: []func(http.Handler) http.Handler{
+			PanicHandler(log),
+		},
+	}
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/line_callback", h.lineCallback())
+	h.registerMiddleware(mux).ServeHTTP(w, r)
+}
+
+func (h *Handler) registerMiddleware(handler http.Handler) http.Handler {
+	for _, middleware := range h.middlewares {
+		handler = middleware(handler)
+	}
+	return handler
+}
+
+func (h *Handler) lineCallback() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := prop.Extract(r.Context(), propagation.HeaderCarrier(r.Header))
+
+		cl := h.log.WithTraceFromContext(ctx)
+		cl.Info("line callback received")
+
+		events, err := h.bot.EventsFromRequest(r)
+		if err != nil {
+			cl.Error("failed to parse request", zap.Error(err))
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		if err := h.eventHandler.Handle(ctx, events); err != nil {
+			cl.Error("failed to handle events", zap.Error(err))
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
---
+++ presentation/http/middleware.go
@@ -0,0 +1,24 @@
+package http
+
+import (
+	"net/http"
+
+	"go.uber.org/zap"
+
+	"github.com/ww24/linebot/logger"
+)
+
+func PanicHandler(log *logger.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					log.Error("paniced in http handler", zap.Any("error", err))
+					http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				}
+			}()
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
