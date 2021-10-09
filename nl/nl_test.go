c78e2aa36141a36b4f605f9d78e1be29d511741a
ww24 <ww24@users.noreply.github.com>
2021-10-10 02:22:17 +0900

    refactor(nl): implements NLParser

--- nl/nl_test.go
+++ nl/nl_test.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/ww24/linebot/domain/model"
 )
 
 func TestParser_Parse(t *testing.T) {
@@ -14,111 +16,111 @@ func TestParser_Parse(t *testing.T) {
 
 	tests := []struct {
 		src  string
-		want *Item
+		want *model.Item
 	}{
 		{
 			src: "りんごを削除",
-			want: &Item{
+			want: &model.Item{
 				Name:   []string{"りんご"},
-				Action: ActionTypeDelete,
+				Action: model.ActionTypeDelete,
 			},
 		},
 		{
 			src: "りんごを削除する。",
-			want: &Item{
+			want: &model.Item{
 				Name:   []string{"りんご"},
-				Action: ActionTypeDelete,
+				Action: model.ActionTypeDelete,
 			},
 		},
 		{
 			src: "りんごを消してください。",
-			want: &Item{
+			want: &model.Item{
 				Name:   []string{"りんご"},
-				Action: ActionTypeDelete,
+				Action: model.ActionTypeDelete,
 			},
 		},
 		{
 			src: "りんごを除去",
-			want: &Item{
+			want: &model.Item{
 				Name:   []string{"りんご"},
-				Action: ActionTypeDelete,
+				Action: model.ActionTypeDelete,
 			},
 		},
 		{
 			src: "1番を削除",
-			want: &Item{
+			want: &model.Item{
 				Indexes: []int{1},
-				Action:  ActionTypeDelete,
+				Action:  model.ActionTypeDelete,
 			},
 		},
 		{
 			src: "１番目を削除。",
-			want: &Item{
+			want: &model.Item{
 				Indexes: []int{1},
-				Action:  ActionTypeDelete,
+				Action:  model.ActionTypeDelete,
 			},
 		},
 		{
 			src: "１を削除",
-			want: &Item{
+			want: &model.Item{
 				Indexes: []int{1},
-				Action:  ActionTypeDelete,
+				Action:  model.ActionTypeDelete,
 			},
 		},
 		{
 			src: "①を削除",
-			want: &Item{
+			want: &model.Item{
 				Indexes: []int{1},
-				Action:  ActionTypeDelete,
+				Action:  model.ActionTypeDelete,
 			},
 		},
 		{
 			src: "1,2,3を削除.",
-			want: &Item{
+			want: &model.Item{
 				Indexes: []int{1, 2, 3},
-				Action:  ActionTypeDelete,
+				Action:  model.ActionTypeDelete,
 			},
 		},
 		{
 			src: "１、２、３を消す。",
-			want: &Item{
+			want: &model.Item{
 				Indexes: []int{1, 2, 3},
-				Action:  ActionTypeDelete,
+				Action:  model.ActionTypeDelete,
 			},
 		},
 		{
 			src: "1と2と3を除去",
-			want: &Item{
+			want: &model.Item{
 				Indexes: []int{1, 2, 3},
-				Action:  ActionTypeDelete,
+				Action:  model.ActionTypeDelete,
 			},
 		},
 		{
 			src: "1と2と3を除去",
-			want: &Item{
+			want: &model.Item{
 				Indexes: []int{1, 2, 3},
-				Action:  ActionTypeDelete,
+				Action:  model.ActionTypeDelete,
 			},
 		},
 		{
 			src: "11を削除",
-			want: &Item{
+			want: &model.Item{
 				Indexes: []int{11},
-				Action:  ActionTypeDelete,
+				Action:  model.ActionTypeDelete,
 			},
 		},
 		{
 			src: "11,12を削除",
-			want: &Item{
+			want: &model.Item{
 				Indexes: []int{11, 12},
-				Action:  ActionTypeDelete,
+				Action:  model.ActionTypeDelete,
 			},
 		},
 		{
 			src: "11と12を削除",
-			want: &Item{
+			want: &model.Item{
 				Indexes: []int{11, 12},
-				Action:  ActionTypeDelete,
+				Action:  model.ActionTypeDelete,
 			},
 		},
 	}
