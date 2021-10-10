b976054ffac254a82aea8c491578918e3046148c
ww24 <ww24@users.noreply.github.com>
2021-10-10 12:52:14 +0900

    fix(domain/model): add UniqueIndexes method into Item

--- domain/model/item.go
+++ domain/model/item.go
@@ -12,3 +12,15 @@ type Item struct {
 	Name    []string
 	Action  ActionType
 }
+
+func (i *Item) UniqueIndexes() []int {
+	indexes := make([]int, 0)
+	set := make(map[int]struct{}, len(i.Indexes))
+	for _, index := range i.Indexes {
+		if _, ok := set[index]; !ok {
+			indexes = append(indexes, index)
+			set[index] = struct{}{}
+		}
+	}
+	return indexes
+}
