c2035d3830b4135235d15361e3306339e75f161a
ww24 <ww24@users.noreply.github.com>
2022-04-09 04:09:02 +0900

    chore(cmd): update wire

--- cmd/linebot/wire_gen.go
+++ cmd/linebot/wire_gen.go
@@ -11,7 +11,9 @@ import (
 	"github.com/ww24/linebot/config"
 	"github.com/ww24/linebot/domain/service"
 	"github.com/ww24/linebot/infra/external/linebot"
+	"github.com/ww24/linebot/infra/external/weather"
 	"github.com/ww24/linebot/infra/firestore"
+	"github.com/ww24/linebot/infra/gcs"
 	"github.com/ww24/linebot/infra/scheduler"
 	"github.com/ww24/linebot/interactor"
 	"github.com/ww24/linebot/nl"
@@ -50,12 +52,31 @@ func register(contextContext context.Context) (*bot, error) {
 		return nil, err
 	}
 	reminderImpl := service.NewReminder(reminder, schedulerScheduler)
-	interactorReminder := interactor.NewReminder(conversationImpl, reminderImpl, messageProviderSet, botImpl)
-	eventHandler, err := interactor.NewEventHandler(shopping, interactorReminder, reminderImpl, messageProviderSet, botImpl, configConfig)
+	interactorReminder := interactor.NewReminder(conversationImpl, reminderImpl, messageProviderSet, botImpl, configConfig)
+	weatherWeather, err := weather.NewWeather(configConfig)
 	if err != nil {
 		return nil, err
 	}
-	handler := http.NewHandler(logger, botImpl, eventHandler)
+	gcsClient, err := gcs.New(contextContext)
+	if err != nil {
+		return nil, err
+	}
+	weatherImageStore, err := gcs.NewWeatherImageStore(gcsClient, configConfig)
+	if err != nil {
+		return nil, err
+	}
+	weatherImpl := service.NewWeather(weatherWeather, weatherImageStore)
+	interactorWeather := interactor.NewWeather(weatherImpl, messageProviderSet, botImpl)
+	eventHandler, err := interactor.NewEventHandler(shopping, interactorReminder, interactorWeather, reminderImpl, messageProviderSet, botImpl, configConfig)
+	if err != nil {
+		return nil, err
+	}
+	imageStore, err := gcs.NewImageStore(gcsClient, configConfig)
+	if err != nil {
+		return nil, err
+	}
+	image := interactor.NewImage(imageStore)
+	handler := http.NewHandler(logger, botImpl, eventHandler, image)
 	mainBot := newBot(configConfig, handler)
 	return mainBot, nil
 }
