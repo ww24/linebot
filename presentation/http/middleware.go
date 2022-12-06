e5b41a5cf0a05df6d04c1b0d1d43ccfa164778b5
ww24 <ww24@users.noreply.github.com>
2022-12-07 03:02:30 +0900

    fix(http): fix access log middleware

--- presentation/http/middleware.go
+++ presentation/http/middleware.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"context"
 	"net/http"
 	"net/textproto"
 	"strings"
@@ -36,47 +37,41 @@ func panicHandler(log *logger.Logger) func(http.Handler) http.Handler {
 func accessLogHandler(publisher accesslog.Publisher) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()
 			start := time.Now()
 
 			accessLog := &avro.AccessLog{
 				Timestamp:    &avro.UnionNullLong{Long: start.UnixMicro(), UnionType: avro.UnionNullLongTypeEnumLong},
 				Id:           &avro.UnionNullString{String: xid.New().String(), UnionType: avro.UnionNullStringTypeEnumString},
-				TraceId:      &avro.UnionNullString{},
-				Ip:           &avro.UnionNullString{},
+				TraceId:      nil,
+				Ip:           nil,
 				UserAgent:    &avro.UnionNullString{String: r.UserAgent(), UnionType: avro.UnionNullStringTypeEnumString},
 				Method:       &avro.UnionNullString{String: r.Method, UnionType: avro.UnionNullStringTypeEnumString},
 				Path:         &avro.UnionNullString{String: r.URL.Path, UnionType: avro.UnionNullStringTypeEnumString},
 				Query:        &avro.UnionNullString{String: r.URL.RawQuery, UnionType: avro.UnionNullStringTypeEnumString},
-				Status:       &avro.UnionNullInt{},
-				Duration:     &avro.UnionNullInt{},
+				Status:       nil,
+				Duration:     nil,
 				RequestSize:  &avro.UnionNullInt{Int: int32(r.ContentLength), UnionType: avro.UnionNullIntTypeEnumInt},
-				ResponseSize: &avro.UnionNullInt{},
+				ResponseSize: nil,
 			}
 
-			if t := trace.SpanContextFromContext(ctx).TraceID(); t.IsValid() {
-				accessLog.TraceId.String = t.String()
-				accessLog.TraceId.UnionType = avro.UnionNullStringTypeEnumString
+			if t := trace.SpanContextFromContext(r.Context()).TraceID(); t.IsValid() {
+				accessLog.TraceId = &avro.UnionNullString{String: t.String(), UnionType: avro.UnionNullStringTypeEnumString}
 			}
 
 			ips := strings.Split(r.Header.Get("X-Forwarded-For"), ",")
 			if len(ips) > trustedProxies {
 				clientIP := textproto.TrimString(ips[len(ips)-trustedProxies-1])
-				accessLog.Ip.String = clientIP
-				accessLog.Ip.UnionType = avro.UnionNullStringTypeEnumString
+				accessLog.Ip = &avro.UnionNullString{String: clientIP, UnionType: avro.UnionNullStringTypeEnumString}
 			}
 
 			rw := newResponseWriter(w)
 			next.ServeHTTP(rw, r)
 
-			accessLog.Status.Int = int32(rw.status)
-			accessLog.Status.UnionType = avro.UnionNullIntTypeEnumInt
-			accessLog.Duration.Int = int32(time.Since(start).Microseconds())
-			accessLog.Duration.UnionType = avro.UnionNullIntTypeEnumInt
-			accessLog.ResponseSize.Int = int32(rw.size)
-			accessLog.ResponseSize.UnionType = avro.UnionNullIntTypeEnumInt
+			accessLog.Status = &avro.UnionNullInt{Int: int32(rw.status), UnionType: avro.UnionNullIntTypeEnumInt}
+			accessLog.Duration = &avro.UnionNullInt{Int: int32(time.Since(start).Microseconds()), UnionType: avro.UnionNullIntTypeEnumInt}
+			accessLog.ResponseSize = &avro.UnionNullInt{Int: int32(rw.size), UnionType: avro.UnionNullIntTypeEnumInt}
 
-			publisher.Publish(ctx, accessLog)
+			publisher.Publish(context.Background(), accessLog)
 		})
 	}
 }
