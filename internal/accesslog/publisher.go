ff26ec4f10f9af1236bc245f5f8a4b7e7b7579da
ww24 <ww24@users.noreply.github.com>
2023-09-18 00:44:51 +0900

    fix(internal/accesslog): add publish error handling

--- internal/accesslog/publisher.go
+++ internal/accesslog/publisher.go
@@ -13,6 +13,8 @@ import (
 	"github.com/ww24/linebot/logger"
 )
 
+const resultChannelSize = 100
+
 type Publisher interface {
 	Publish(context.Context, *avro.AccessLog)
 }
@@ -22,7 +24,8 @@ type NoopPublisher struct{}
 func (*NoopPublisher) Publish(context.Context, *avro.AccessLog) {}
 
 type PubSubPublisher struct {
-	topic *pubsub.Topic
+	topic   *pubsub.Topic
+	results chan *pubsub.PublishResult
 }
 
 func NewPublisher(p *pubsub.Client, cfg *config.AccessLog) (Publisher, func()) {
@@ -32,19 +35,41 @@ func NewPublisher(p *pubsub.Client, cfg *config.AccessLog) (Publisher, func()) {
 
 	topic := p.Topic(cfg.Topic)
 	topic.PublishSettings.DelayThreshold = time.Second
-	return &PubSubPublisher{
-		topic: topic,
-	}, topic.Stop
+	publisher := &PubSubPublisher{
+		topic:   topic,
+		results: make(chan *pubsub.PublishResult, resultChannelSize),
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	go publisher.worker(ctx)
+	stop := func() {
+		topic.Stop()
+		cancel()
+	}
+	return publisher, stop
 }
 
 func (p *PubSubPublisher) Publish(ctx context.Context, al *avro.AccessLog) {
 	buf := new(bytes.Buffer)
+	dl := logger.Default(ctx)
 	if err := al.Serialize(buf); err != nil {
-		dl := logger.Default(ctx)
-		dl.Error("failed to serialize access log", zap.Error(err))
+		dl.Error("accesslog: failed to serialize access log", zap.Error(err))
 		return
 	}
-	p.topic.Publish(ctx, &pubsub.Message{
+	result := p.topic.Publish(ctx, &pubsub.Message{
 		Data: buf.Bytes(),
 	})
+	select {
+	case p.results <- result:
+	default:
+		dl.Info("accesslog: publish results is sampled")
+	}
+}
+
+func (p *PubSubPublisher) worker(ctx context.Context) {
+	dl := logger.Default(ctx)
+	for result := range p.results {
+		if _, err := result.Get(ctx); err != nil {
+			dl.Error("accesslog: failed to publish access log", zap.Error(err))
+		}
+	}
 }
